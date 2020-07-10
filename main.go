22d982cd08fa22c2b930386d0831648d41f8adcc
Xue Jiao <jiao.xue@shopee.com>
2020-07-10 20:04:23 +0800

    refactor

--- main.go
+++ main.go
@@ -62,53 +62,44 @@ func GetMTU(name string) (int, error) {
 // no data is available, it will block in a poll() syscall until the file
 // descriptor becomes readable.
 func BlockingRead(fd int, b []byte) (int, error) {
-    for {
-        log.Println("before read")
-        n, _, e := syscall.RawSyscall(syscall.SYS_READ, uintptr(fd), uintptr(unsafe.Pointer(&b[0])), uintptr(len(b)))
-        if e == 0 {
-            log.Printf("read error %q", e)
-            return int(n), nil
-        }
+	for {
+		n, _, e := syscall.RawSyscall(syscall.SYS_READ, uintptr(fd), uintptr(unsafe.Pointer(&b[0])), uintptr(len(b)))
+		if e == 0 {
+			return int(n), nil
+		}
 
-        log.Printf("read n %d", n)
-        log.Printf("b is %q", b)
+		event := struct {
+			fd      int32
+			events  int16
+			revents int16
+		}{
+			fd:     int32(fd),
+			events: 1, // POLLIN
+		}
 
-        event := struct {
-            fd int32
-            events int16
-            revents int16
-        }{
-            fd: int32(fd),
-            events: 1, //POLLIN
-        }
-
-        _, _, e = syscall.Syscall(syscall.SYS_POLL, uintptr(unsafe.Pointer(&event)), 1, uintptr(math.MaxUint64))
-        if e != 0 && e != syscall.EINTR {
-            return 0, e
-        }
-    }
+		_, _, e = syscall.Syscall(syscall.SYS_POLL, uintptr(unsafe.Pointer(&event)), 1, uintptr(math.MaxUint64))
+		if e != 0 && e != syscall.EINTR {
+			return 0, e
+		}
+	}
 }
 
 func NonBlockingWrite(fd int, buf []byte) error {
-    var ptr unsafe.Pointer
-    if len(buf) > 0 {
-        ptr = unsafe.Pointer(&buf[0])
-    }
+	var ptr unsafe.Pointer
+	if len(buf) > 0 {
+		ptr = unsafe.Pointer(&buf[0])
+	}
 
-    n, _, e := syscall.RawSyscall(syscall.SYS_WRITE, uintptr(fd), uintptr(ptr), uintptr(len(buf)))
+	n, _, e := syscall.RawSyscall(syscall.SYS_WRITE, uintptr(fd), uintptr(ptr), uintptr(len(buf)))
+	if e != 0 {
+		return e
+	}
 
-    if e != 0 {
-        log.Printf("write error %q", e)
-        return e
-    }
+	if n != uintptr(len(buf)) {
+		return fmt.Errorf("wrong number of bytes written: expected %d, got %d", len(buf), n)
+	}
 
-    log.Printf("written %d", n)
-
-    if n != uintptr(len(buf)) {
-        return fmt.Errorf("wrong number of bytes written: expected %d, got %d", len(buf), n)
-    }
-
-    return nil
+	return nil
 }
 
 func main() {
@@ -127,9 +118,10 @@ func main() {
     }
     log.Printf("mtu fd is %d", mtu)
 
-    b := make([]byte, 100)
+    b := make([]byte, 20)
     go BlockingRead(fd, b)
     go NonBlockingWrite(fd, []byte{'h', 'e', 'l', 'l', 'o'})
 
     time.Sleep(10 * 1000 * time.Millisecond)
+    log.Println(b)
 }
