2b76e47e21fece12b128b2346037c2caad6d0a3e
Xue Jiao <jiao.xue@shopee.com>
2020-07-10 19:12:48 +0800

    Add GetMTU

--- main.go
+++ main.go
@@ -34,6 +34,27 @@ func Open(name string) (int, error) {
     return fd, nil
 }
 
+func GetMTU(name string) (int, error) {
+    fd, err := syscall.Socket(syscall.AF_UNIX, syscall.SOCK_DGRAM, 0)
+    if err != nil {
+        return 0, err
+    }
+    defer syscall.Close(fd)
+
+    var ifreq struct {
+        name [16]byte
+        mtu int32
+        _ [20]byte
+    }
+    copy(ifreq.name[:], name)
+    _, _, errno := syscall.Syscall(syscall.SYS_IOCTL, uintptr(fd), syscall.SIOCGIFMTU, uintptr(unsafe.Pointer(&ifreq)))
+    if errno != 0 {
+        return 0, errno
+    }
+
+    return int(ifreq.mtu), nil
+}
+
 func main() {
     log.Println("start")
     name := "tun0"
@@ -43,4 +64,10 @@ func main() {
     }
 
     log.Printf("opened fd is %q", fd)
+
+    mtu, err := GetMTU("tun0")
+    if err != nil {
+        log.Printf("error = %q", err)
+    }
+    log.Printf("mtu fd is %q", mtu)
 }
